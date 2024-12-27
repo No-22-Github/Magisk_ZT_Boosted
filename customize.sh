8020f3db33b4d74cf88ecb26c50d6b215b888339
No.22 <132265925+No-22-Github@users.noreply.github.com>
2024-12-28 00:57:08 +0800

    update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,11 +1,12 @@
 ui_print "#############################################"
 ui_print "您正在安装的是 ZT_Boosted 子腾系统通用优化模块"
 ui_print "模块版本：v1.0-dev"
-ui_print "   ___         _  __       ___  ___ "
-ui_print "  / _ )__ __  / |/ /__    |_  ||_  |"
-ui_print " / _  / // / /    / _ \_ / __// __/ "
-ui_print "/____/\_, / /_/|_/\___(_)____/____/ "
-ui_print "     /___/                          "
+echo '   ___         _  __       ___  ___ '
+echo '  / _ )__ __  / |/ /__    |_  ||_  |'
+echo ' / _  / // / /    / _ \_ / __// __/ '
+echo '/____/\_, / /_/|_/\___(_)____/____/ '
+echo '     /___/                          '
+ui_print "博客：no22.top"
 ui_print "#############################################"
 echo -e "系统版本：$(getprop ro.build.display.id)"
 ui_print "- 正在释放文件"
@@ -15,11 +16,11 @@ ui_print "- 配置文件与日志位于 /storage/emulated/0/Android/ZT_boost"
 unzip -o "$ZIPFILE" 'config.yaml' -d "/storage/emulated/0/Android/ZT_Boosted/" >&2
 echo "[$(date '+%m-%d %H:%M:%S.%3N')] ZT_Boosted v1.0-dev 模块安装成功, 等待重启" >> "/storage/emulated/0/Android/ZT_Boosted/config.yaml.log"
 ui_print "- ZT_Boosted v1.0-dev 子腾系统通用优化模块"
-ui_print "- 对系统 CPU / GPU / 内存深度优化"
-ui_print "- 模块可以释放手机的全部性能"
-ui_print "- 可自行查看 service.sh 与 system.prop 文件"
-ui_print "- 默认不删除温控, 关于打开“删除温控”功能请自行前往酷安查看教程"
-ui_print "- 配置文件在 /storage/emulated/0/Android/config.yaml"
-ui_print "- 日志文件在 /storage/emulated/0/Android/config.yaml.log"
-ui_print "- 作者 QQ: 854675824 | 酷安@TheCheese"
+ui_print "- 开启无线 ADB 解除安装限制"
+ui_print "- 针对手表 CPU / GPU / 内存深度优化"
+ui_print "- 屏幕优化 触控增强 帧率更稳定"
+ui_print "- 网络 TCP 优化 连接更稳定 速率增加"
+ui_print "- 配置文件在 /storage/emulated/0/Android/ZT_Boosted/config.yaml"
+ui_print "- 日志文件在 /storage/emulated/0/Android/ZT_Boosted/config.yaml.log"
+ui_print "- QQ:  | 酷安@TheCheese"
 ui_print "- 模块安装结束 重启生效"
